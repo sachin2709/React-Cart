c6b0d8b3f5e44aeccec7a91bedf30a4042f92a6a
sachin2709 <sachinmishra2709@gmail.com>
2022-11-01 18:39:30 +0530

    adding files

---
+++ src/CartItem.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class CartItem extends React.Component {
+  render () {
+    const {qty, price, title } = this.props.product;
+    const { product } = this.props;
+
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img
+            style={{ height: 110, width: 110, borderRadius: 5, background: '#ccc' }}
+            src={product.img}
+          />
+        </div>
+        <div className="right-block">
+          <div style={{ fontSize: 25 }}> {title} </div>
+          <div style={{ color: '#777' }}>{price}</div>
+          <div style={{ color: '#777' }}>Qty: {qty}</div>
+          <div className="cart-item-actions">
+            <img 
+              alt="add"
+              src="https://cdn-icons-png.flaticon.com/128/3024/3024515.png"
+              className="action-icons"
+              onClick={() => this.props.onIncreaseQuantity(product)}
+            />
+            <img
+              alt="minus"
+              src="https://cdn-icons-png.flaticon.com/128/3941/3941860.png"
+              className="action-icons"
+              onClick={() => this.props.onDecreaseQuantity(product)}
+            />
+            <img
+              alt="delete"
+              src="https://cdn-icons-png.flaticon.com/128/484/484662.png"
+              className="action-icons"
+              onClick={() => this.props.onDeleteProduct(product.id)}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CartItem;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQA1GAGbUhlDHHW9aIbE-tV6YYK42C8Yk",
+  authDomain: "cart-b11bb.firebaseapp.com",
+  projectId: "cart-b11bb",
+  storageBucket: "cart-b11bb.appspot.com",
+  messagingSenderId: "1012741297574",
+  appId: "1:1012741297574:web:0895012ce6ed7db8f82b67"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
+
